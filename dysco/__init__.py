4dbc0fe44979bdafeabdcba73f3d09df711ccd11
Evan Sangaline <esangaline@gmail.com>
2019-11-16 17:11:31 -0500

    Bump the project version to v0.0.8.

--- dysco/__init__.py
+++ dysco/__init__.py
@@ -1,6 +1,6 @@
 from dysco.dysco import Dysco
 
-__version__ = '0.0.7'
+__version__ = '0.0.8'
 __author__ = 'Evan Sangaline <evan@intoli.com>'
 __description__ = 'Dysco provides configurable dynamic scoping behavior in Python.'
 __all__ = ['Dysco', 'g']
