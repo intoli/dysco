8196baf4c20fefd1760bebab561d9b32424013fe
Evan Sangaline <esangaline@gmail.com>
2019-11-16 15:35:49 -0500

    Add support for stack levels and item access.

--- dysco/dysco.py
+++ dysco/dysco.py
@@ -1,26 +1,41 @@
 """Houses the implementation of the main ``Dysco`` class and project API."""
 import inspect
-from typing import Any
+from typing import Any, Hashable
 
 from dysco.scope import Scope, find_parent_scope
 
 
 class Dysco:
-    def __init__(self, read_only: bool = False):
+    def __init__(self, read_only: bool = False, stacklevel: int = 1):
         self.__read_only = read_only
+        self.__stacklevel = stacklevel
 
     def __getattr__(self, attribute: str) -> Any:
         if attribute.startswith('_Dysco_'):
             return super().__getattribute__(attribute)
+
+        try:
+            current_frame = inspect.currentframe()
+            self.__stacklevel += 1
+            return self[attribute]
+        except KeyError:
+            raise AttributeError(f'The attribute {attribute} was not found in any scope.')
+        finally:
+            self.__stacklevel -= 1
+            # Delete the current frame to avoid reference cycles.
+            del current_frame
+
+    def __getitem__(self, key: Hashable) -> Any:
         stack = inspect.stack()
-        current_frame = stack.pop(0).frame
+        current_frame = stack[0].frame
+        stack = stack[self.__stacklevel :]
         try:
             scope = Scope(stack[0].frame, namespace=hex(id(self)))
             while scope:
-                if attribute in scope.variables:
-                    return scope.variables[attribute]
+                if key in scope.variables:
+                    return scope.variables[key]
                 scope, stack = find_parent_scope(scope, stack)
-            raise AttributeError(f'The attribute {attribute} was not found in any scope.')
+            raise KeyError(f'The key "{key}" was not found in any scope.')
         finally:
             # Delete the current frame to avoid reference cycles.
             del current_frame
@@ -28,18 +43,31 @@ class Dysco:
     def __setattr__(self, attribute: str, value: Any) -> None:
         if attribute.startswith('_Dysco_'):
             super().__setattr__(attribute, value)
+            return
+
+        try:
+            current_frame = inspect.currentframe()
+            self.__stacklevel += 1
+            self[attribute] = value
+        finally:
+            self.__stacklevel -= 1
+            # Delete the current frame to avoid reference cycles.
+            del current_frame
+
+    def __setitem__(self, key: str, value: Any) -> None:
         read_only = getattr(self, '_Dysco_read_only', False)
         stack = inspect.stack()
-        current_frame = stack.pop(0).frame
+        current_frame = stack[0].frame
+        stack = stack[self.__stacklevel :]
         try:
             initial_scope = Scope(stack[0].frame, namespace=hex(id(self)))
             scope = initial_scope
             while not read_only and scope:
-                if attribute in scope.variables:
-                    scope.variables[attribute] = value
+                if key in scope.variables:
+                    scope.variables[key] = value
                     return
                 scope, stack = find_parent_scope(scope, stack)
-            initial_scope.variables[attribute] = value
+            initial_scope.variables[key] = value
         finally:
             # Delete the current frame to avoid reference cycles.
             del current_frame
